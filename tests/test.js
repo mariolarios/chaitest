f4b4342ac4399eb6421827e0ea8f9c8c2ad50d46
mariolarios <108195141+mariolarios@users.noreply.github.com>
2023-02-03 17:01:18 -0500

    answers for lesson 15

--- tests/test.js
+++ tests/test.js
@@ -17,29 +17,61 @@ describe("People", () => {
         .send({ age: 10 })
         .end((err, res) => {
           res.should.have.status(400);
-          res.body.should.be.eql({ error: "Please enter a name." });
+          res.body.should.be.eql({ error: "enter a name." });
           done();
         });
     });
     it("should create a people entry with valid input", (done) => {
       // your code goes here
-      done();
+      chai
+        .request(app)
+        .post("/api/v1/people")
+        .send({ name: "russ", age: 37 })
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.contain({ message: "A person record was added" });
+          this.lastindex = res.body.index;
+          done();
+        });
     });
   });
   describe("get /api/v1/people", () => {
-    it(`should return an array of person entries of length ${this.lastIndex + 1}`, (done) => {
-           // your code goes here
-           done();
+    it(`should return an array of person entries of length ${
+      this.lastIndex + 1
+    }`, (done) => {
+      // your code goes here
+      chai
+        .request(app)
+        .get("/api/v1/people")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.length(this.lastIndex + 1);
+          done();
+        });
     });
   });
   describe("get /apl/v1/people/:id", () => {
     it("should return the entry corresponding to the last person added.", (done) => {
       // your code goes here
-      done();
+      chai
+        .request(app)
+        .get(`/api/v1/people/${this.lastIndex}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          console.log(res.body);
+          res.body.name.should.be.eql("russ");
+          done();
+        });
     });
     it("should return an error if the index is >= the length of the array", (done) => {
-            // your code goes here
-            done();
+      // your code goes here
+      chai
+        .request(app)
+        .get("/api/v1/people/43")
+        .end((err, res) => {
+          res.should.have.status(404);
+          done();
+        });
     });
   });
 });
